432283fbbc46898de5242febaf0ac3cabcd0b7b1
jiangchaochao <30774951+jiangchaochao@users.noreply.github.com>
2017-11-03 09:20:05 +0800

    Initial commit

---
+++ app/src/main/java/jiangc/cn/yview/camera2/Preview.java
@@ -0,0 +1,39 @@
+package jiangc.cn.yview.camera2;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.util.Arrays;
+
+/**
+ * Created by Administrator on 2017/11/2.
+ *
+ */
+
+public class Preview extends TextureView  {
+    private String TAG = "Preview";
+    public Preview(Context context, SurfaceTextureListener listener) {
+        super(context);
+        setSurfaceTextureListener(listener);
+    }
+
+}
